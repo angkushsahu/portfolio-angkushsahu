56be60aec8c26f257446458172c43e2d52379da8
Angkush <angkysahu2502@gmail.com>
2023-03-19 15:44:57 +0530

    update: splash-screen and smooth routing animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,32 @@
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import { RouterProvider } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { routing } from "components/app";
-import { Loading } from "components";
+import { Loading, SplashScreen } from "components";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function App() {
+    const sessionItem = "splashScreen";
+    const showFlashScreen = sessionStorage.getItem(sessionItem);
+    const [splash, setSplash] = useState(showFlashScreen ? false : true);
+    setTimeout(() => {
+        setSplash(false);
+        sessionStorage.setItem(sessionItem, "true");
+    }, 3800);
+
     return (
         <main className="parent_root">
             <div className="shape_1"></div>
             <div className="shape_2"></div>
-            <div className="child_root">
-                <Suspense fallback={<Loading />}>
-                    <RouterProvider router={routing} />
-                </Suspense>
-            </div>
+            {splash ? (
+                <SplashScreen />
+            ) : (
+                <div className="child_root">
+                    <Suspense fallback={<Loading />}>
+                        <RouterProvider router={routing} />
+                    </Suspense>
+                </div>
+            )}
             <ToastContainer position="top-center" theme="dark" />
         </main>
     );
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Header } from "./header";
 export { default as Loading } from "./loading";
+export { default as SplashScreen } from "./splash";
